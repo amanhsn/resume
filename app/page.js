28f2f6ef18ca13b1385e1c4cfafefe433e8fec6d
amanhsn <zilleh67@gmail.com>
2025-01-21 20:04:26 +0500

    'Read.cv'

--- app/page.js
+++ app/page.js
@@ -1,101 +1,198 @@
-import Image from "next/image";
+'use client';
+import React from 'react';
+import './globals.css';
 
-export default function Home() {
+const Resume = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="Wrapper">
+      <div className="inWrapper">
+        <div className="profileHeader" bis_skin_checked="1">
+          <div
+            data-status="false"
+            data-editable="false"
+            className="profilePhoto"
+            bis_skin_checked="1"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <div
+              className="profilePhotoDetail"
+              data-size="large"
+              style={{ width: '92px', height: '92px', margin: 0 }}
+              bis_skin_checked="1"
+            >
+              <img
+                draggable="false"
+                alt="Aman Hussain"
+                src="/aman.jpg"
+                srcSet="/aman.jpg 1x, /aman@2x.jpg 2x, /aman@3x.jpg 3x"
+                style={{ opacity: 1 }}
+              />
+            </div>
+          </div>
+          <div className="profileInfo" bis_skin_checked="1">
+            <div className="profileInfoName" bis_skin_checked="1">
+              <h2>Aman Hussain</h2>
+            </div>
+            <div style={{ display: 'flex', flexDirection: 'row' }} bis_skin_checked="1">
+              <p>Product Design in Islamabad, PK, he/him</p>
+            </div>
+            <div className="profileBadge" bis_skin_checked="1">
+              <div data-custom-domain="false" className="badgeLink" bis_skin_checked="1">
+                <a className="MegaLink_megalink__u_TFB" href="http://amanhsn.framer.website" target="_blank" rel="noopener noreferrer" data-hover="false">
+                  amanhsn.com
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <section data-print="true" id="Profile_aboutID__Z2GAs" style={{opacity: 1}}>
+        <h3>About</h3>
+        <div className="Profile_about__Q_Z_r" bis_skin_checked="1">
+        <p>Adaptable digital product designer with a flair for visual creativity. My background in visual design helps me create pragmatic solutions to complex problems, focussed on delighting users.</p>
+        </div>
+        </section>
+        <section data-print="true" id="contact" style={{opacity: 1}}>
+        <div class="Profile_breakInside__ZPmEM" bis_skin_checked="1">
+        <h3>Contact</h3>
+        <div className="ContactItem_contact__9zpDH" bis_skin_checked="1">
+        <div className="ContactItem_platform__6b5md" bis_skin_checked="1">
+        <span>Email</span></div><div className="ContactItem_platformLink___dB19" bis_skin_checked="1">
+        <div bis_skin_checked="1">
+        <a className="MegaLink_megalink__u_TFB" href="mailto:hello@ellencovey.com" target="_blank" rel="noopener noreferrer me" data-hover="true">syedamanhsn@gmail.com</a>
+        <span style={{whiteSpace: 'nowrap'}}>﻿<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M3.5 3C3.22386 3 3 3.22386 3 3.5C3 3.77614 3.22386 4 3.5 4V3ZM8.5 3.5H9C9 3.22386 8.77614 3 8.5 3V3.5ZM8 8.5C8 8.77614 8.22386 9 8.5 9C8.77614 9 9 8.77614 9 8.5H8ZM2.64645 8.64645C2.45118 8.84171 2.45118 9.15829 2.64645 9.35355C2.84171 9.54882 3.15829 9.54882 3.35355 9.35355L2.64645 8.64645ZM3.5 4H8.5V3H3.5V4ZM8 3.5V8.5H9V3.5H8ZM8.14645 3.14645L2.64645 8.64645L3.35355 9.35355L8.85355 3.85355L8.14645 3.14645Z" fill="#eee">
+        </path></svg></span></div></div></div></div>
+        <div className="ContactItem_contact__9zpDH" bis_skin_checked="1">
+        <div className="ContactItem_platform__6b5md" bis_skin_checked="1">
+        <span>Instagram</span>
+        </div>
+        <div className="ContactItem_platformLink___dB19" bis_skin_checked="1">
+        <div bis_skin_checked="1">
+        <a className="MegaLink_megalink__u_TFB" href="https://instagram.com/aman.h" target="_blank" rel="noopener noreferrer me" data-hover="true">aman.h_</a>
+        <span style={{whiteSpace: 'nowrap'}}>﻿<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3.5 3C3.22386 3 3 3.22386 3 3.5C3 3.77614 3.22386 4 3.5 4V3ZM8.5 3.5H9C9 3.22386 8.77614 3 8.5 3V3.5ZM8 8.5C8 8.77614 8.22386 9 8.5 9C8.77614 9 9 8.77614 9 8.5H8ZM2.64645 8.64645C2.45118 8.84171 2.45118 9.15829 2.64645 9.35355C2.84171 9.54882 3.15829 9.54882 3.35355 9.35355L2.64645 8.64645ZM3.5 4H8.5V3H3.5V4ZM8 3.5V8.5H9V3.5H8ZM8.14645 3.14645L2.64645 8.64645L3.35355 9.35355L8.85355 3.85355L8.14645 3.14645Z" fill="#eee">
+        </path></svg></span></div></div></div>
+        <div className="ContactItem_contact__9zpDH" bis_skin_checked="1">
+        <div className="ContactItem_platform__6b5md" bis_skin_checked="1">
+        <span>LinkedIn</span></div><div className="ContactItem_platformLink___dB19" bis_skin_checked="1">
+        <div bis_skin_checked="1">
+        <a className="MegaLink_megalink__u_TFB" href="https://linkedin.com/in/amanhsn" target="_blank" rel="noopener noreferrer me" data-hover="true">amanhussain</a>
+        <span style={{whiteSpace: 'nowrap'}}>﻿<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M3.5 3C3.22386 3 3 3.22386 3 3.5C3 3.77614 3.22386 4 3.5 4V3ZM8.5 3.5H9C9 3.22386 8.77614 3 8.5 3V3.5ZM8 8.5C8 8.77614 8.22386 9 8.5 9C8.77614 9 9 8.77614 9 8.5H8ZM2.64645 8.64645C2.45118 8.84171 2.45118 9.15829 2.64645 9.35355C2.84171 9.54882 3.15829 9.54882 3.35355 9.35355L2.64645 8.64645ZM3.5 4H8.5V3H3.5V4ZM8 3.5V8.5H9V3.5H8ZM8.14645 3.14645L2.64645 8.64645L3.35355 9.35355L8.85355 3.85355L8.14645 3.14645Z" fill="#eee"></path></svg></span></div></div></div>
+        </section>
+        <section data-print="true" id="work-experience" style={{ opacity: 1 }}>
+  <div className="Profile_breakInside__ZPmEM" bis_skin_checked="1">
+    <h3>Work Experience</h3>
+
+    {/* Job 1 */}
+    <div className="ReplyContainer_replyContainerWrap__eWAYB" data-confirm="false" data-reply="false" data-admin="false" data-noreply="false" data-desktop="true" bis_skin_checked="1">
+      <div className="ProfileItem_experience__YXtvH" data-flag="profileItem" bis_skin_checked="1">
+        <div className="ProfileItem_duration__utkpJ" bis_skin_checked="1">
+          <span>2023 — Now</span>
+        </div>
+        <div className="ProfileItem_experienceContent__0E9pa" bis_skin_checked="1">
+          <div bis_skin_checked="1">
+            Product Designer<span> at komoot</span>
+          </div>
+          <div className="ProfileItem_description__9_b0E" bis_skin_checked="1">
+            <p>Working to make outdoor adventures accessible to all.</p>
+          </div>
+        </div>
+      </div>
+      <div className="ReplyContainer_replyContainer__5_fvu" bis_skin_checked="1">
+        <div className="Popover_popoverWrap__WBCCw" bis_skin_checked="1">
+          <button className="Button_button__AQ1KL" data-style="light" data-loading="false">
+            <div className="Button_icon___WOGE" style={{ margin: '0px auto' }} bis_skin_checked="1">
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M11.2927 19.1631C11.824 19.1631 12.2068 18.7725 12.2068 18.249V15.2646H12.4333C15.4646 15.2646 17.3708 16.0303 18.738 18.585C19.0115 19.085 19.3708 19.1631 19.699 19.1631C20.113 19.1631 20.5037 18.7881 20.5037 18.1162C20.5037 12.3428 18.0583 8.74121 12.4333 8.74121H12.2068V5.78809C12.2068 5.26465 11.824 4.83496 11.2771 4.83496C10.8943 4.83496 10.6365 4.99902 10.2224 5.38965L4.05835 11.1553C3.75366 11.4443 3.65991 11.7334 3.65991 11.999C3.65991 12.2568 3.76147 12.5537 4.05835 12.835L10.2224 18.6553C10.5974 19.0068 10.9099 19.1631 11.2927 19.1631Z" fill="#888"></path>
+              </svg>
+            </div>
+          </button>
+        </div>
+      </div>
+    </div>
+
+    {/* Job 2 */}
+    <div className="ReplyContainer_replyContainerWrap__eWAYB" data-confirm="false" data-reply="false" data-admin="false" data-noreply="false" data-desktop="true" bis_skin_checked="1">
+      <div className="ProfileItem_experience__YXtvH" data-flag="profileItem" bis_skin_checked="1">
+        <div className="ProfileItem_duration__utkpJ" bis_skin_checked="1">
+          <span>2019 — 2022</span>
+        </div>
+        <div className="ProfileItem_experienceContent__0E9pa" bis_skin_checked="1">
+          <div className="ProfileItem_title__rsz3J" bis_skin_checked="1">
+            <a className="MegaLink_megalink__u_TFB ProfileItem_titleLink__qXU7l" href="http://ellencovey.com" target="_blank" rel="noopener noreferrer" data-hover="true">
+              Freelance Designer<span> at Ellen Covey</span>
+            </a>
+            <span style={{ whiteSpace: 'nowrap' }}>
+              <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3.5 3C3.22386 3 3 3.22386 3 3.5C3 3.77614 3.22386 4 3.5 4V3ZM8.5 3.5H9C9 3.22386 8.77614 3 8.5 3V3.5ZM8 8.5C8 8.77614 8.22386 9 8.5 9C8.77614 9 9 8.77614 9 8.5H8ZM2.64645 8.64645C2.45118 8.84171 2.45118 9.15829 2.64645 9.35355C2.84171 9.54882 3.15829 9.54882 3.35355 9.35355L2.64645 8.64645ZM3.5 4H8.5V3H3.5V4ZM8 3.5V8.5H9V3.5H8ZM8.14645 3.14645L2.64645 8.64645L3.35355 9.35355L8.85355 3.85355L8.14645 3.14645Z" fill="#eee"></path>
+              </svg>
+            </span>
+          </div>
+          <div className="ProfileItem_description__9_b0E" bis_skin_checked="1">
+            <p>I've worked with a variety of clients, from local startups to global corporations. Previous projects include websites designed and built, full service branding, children's books, seasonal campaigns, editorial design, and more.</p>
+          </div>
+        </div>
+      </div>
+      <div className="ReplyContainer_replyContainer__5_fvu" bis_skin_checked="1">
+        <div className="Popover_popoverWrap__WBCCw" bis_skin_checked="1">
+          <button className="Button_button__AQ1KL" data-style="light" data-loading="false">
+            <div className="Button_icon___WOGE" style={{ margin: '0px auto' }} bis_skin_checked="1">
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M11.2927 19.1631C11.824 19.1631 12.2068 18.7725 12.2068 18.249V15.2646H12.4333C15.4646 15.2646 17.3708 16.0303 18.738 18.585C19.0115 19.085 19.3708 19.1631 19.699 19.1631C20.113 19.1631 20.5037 18.7881 20.5037 18.1162C20.5037 12.3428 18.0583 8.74121 12.4333 8.74121H12.2068V5.78809C12.2068 5.26465 11.824 4.83496 11.2771 4.83496C10.8943 4.83496 10.6365 4.99902 10.2224 5.38965L4.05835 11.1553C3.75366 11.4443 3.65991 11.7334 3.65991 11.999C3.65991 12.2568 3.76147 12.5537 4.05835 12.835L10.2224 18.6553C10.5974 19.0068 10.9099 19.1631 11.2927 19.1631Z" fill="#888"></path>
+              </svg>
+            </div>
+          </button>
+        </div>
+      </div>
+    </div>
+
+    {/* Job 3 */}
+    <div className="ReplyContainer_replyContainerWrap__eWAYB" data-confirm="false" data-reply="false" data-admin="false" data-noreply="false" data-desktop="true" bis_skin_checked="1">
+      <div className="ProfileItem_experience__YXtvH" data-flag="profileItem" bis_skin_checked="1">
+        <div className="ProfileItem_duration__utkpJ" bis_skin_checked="1">
+          <span>2019 — 2021</span>
+        </div>
+        <div className="ProfileItem_experienceContent__0E9pa" bis_skin_checked="1">
+          <div bis_skin_checked="1">
+            Freelance Designer
+          </div>
+          <div className="ProfileItem_description__9_b0E" bis_skin_checked="1">
+            <p>Worked on a variety of freelance projects, providing design services for clients in different industries.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    {}
+    <div className="ReplyContainer_replyContainerWrap__eWAYB" data-confirm="false" data-reply="false" data-admin="false" data-noreply="false" data-desktop="true" bis_skin_checked="1">
+  <div className="ProfileItem_experience__YXtvH" data-flag="profileItem" bis_skin_checked="1">
+    <div className="ProfileItem_duration__utkpJ" bis_skin_checked="1">
+      <span>2019 — 2021</span>
+    </div>
+    <div className="ProfileItem_experienceContent__0E9pa" bis_skin_checked="1">
+      <div bis_skin_checked="1">Visual Designer<span> at MarcoPolo Learning</span></div>
+      <div className="ProfileItem_location__5TQPF" bis_skin_checked="1">London</div>
+      <div className="ProfileItem_description__9_b0E" bis_skin_checked="1">
+        <p>As the visual designer on a lean, cross-functional team building an award-winning children's educational app, I was responsible for a wide range of design projects. These included restructuring and redesigning the sign-up flow to increase install-to-trial rates, designing initial touch-points for the product (such as app store screens and in-app onboarding) as well as a variety of systems, materials and visuals to uphold and enhance the brand.</p>
+      </div>
+      <div className="ProfileItem_images__Z9kAd" bis_skin_checked="1"></div>
+    </div>
+  </div>
+  <div className="ReplyContainer_replyContainer__5_fvu" bis_skin_checked="1">
+    <div className="Popover_popoverWrap__WBCCw" bis_skin_checked="1">
+      <button className="Button_button__AQ1KL" data-style="light" data-loading="false">
+        <div className="Button_icon___WOGE" style={{ margin: '0px auto' }} bis_skin_checked="1">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M11.2927 19.1631C11.824 19.1631 12.2068 18.7725 12.2068 18.249V15.2646H12.4333C15.4646 15.2646 17.3708 16.0303 18.738 18.585C19.0115 19.085 19.3708 19.1631 19.699 19.1631C20.113 19.1631 20.5037 18.7881 20.5037 18.1162C20.5037 12.3428 18.0583 8.74121 12.4333 8.74121H12.2068V5.78809C12.2068 5.26465 11.824 4.83496 11.2771 4.83496C10.8943 4.83496 10.6365 4.99902 10.2224 5.38965L4.05835 11.1553C3.75366 11.4443 3.65991 11.7334 3.65991 11.999C3.65991 12.2568 3.76147 12.5537 4.05835 12.835L10.2224 18.6553C10.5974 19.0068 10.9099 19.1631 11.2927 19.1631Z" fill="#888"></path>
+          </svg>
+        </div>
+      </button>
+    </div>
+  </div>
+</div>
+  </div>
+</section>
+      </div>
     </div>
   );
-}
+};
+
+export default Resume;
